6d0c036ebbff11fbac07a7ed586eafe2521dd533
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2024-10-23 18:09:43 -0400

    attempt to fix routing

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import {useState} from 'react';
 import {MD3LightTheme as DefaultTheme, Provider as PaperProvider} from 'react-native-paper'
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import DashboardTab from './app/(tabs)/Dashboard'; 
 import LoginTab from './app/(auth)/Login'; 
+import SignupTab from './app/(auth)/Signup'; 
+import ForgotPasswordTab from './app/(auth)/ForgotPassword'; 
 import InventoryTab from './app/(tabs)/Inventory'; 
 import ShoppingListTab from './app/(tabs)/ShoppingList'; 
 import UserSettingsTab from './app/(tabs)/UserSettings'; 
@@ -28,7 +30,7 @@ const theme = {
 
 function MainTabs() {
     return (
-      <Tab.Navigator>
+      <Tab.Navigator initialRouteName={"Home"}>
         <Tab.Screen name="Home" component={DashboardTab} />
         <Tab.Screen name="User Settings" component={UserSettingsTab} />
         <Tab.Screen name="Inventory" component={InventoryTab} />
@@ -38,17 +40,30 @@ function MainTabs() {
   }
   
 
-  export default function App() {
+ function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     return (
         <PaperProvider theme={theme}>
             <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen name="Login" component={LoginTab} />
-                    {/* <Stack.Screen name="Signup" component={SignupTab} />
-                    <Stack.Screen name="Forgot Password" component={ForgotPasswordTab} /> */}
-                    <Stack.Screen name="MainTabs" component={MainTabs} />
+                <Stack.Navigator initialRouteName={isLoggedIn ? "MainTabs": "Login"}>
+                    {
+                        !isLoggedIn ? (
+                            <>
+                                <Stack.Screen name="Login" component={LoginTab} />
+                                <Stack.Screen name="Signup" component={SignupTab} />
+                                <Stack.Screen name="Forgot Password" component={ForgotPasswordTab} />
+                            </>
+                        ) : (
+                            <Stack.Screen name="MainTabs" component={MainTabs} options={{ headerShown: false }}/>
+                        )
+                        
+                    }
                 </Stack.Navigator>
             </NavigationContainer>
           </PaperProvider>
     );
   }
+
+
+  export default App;
---
+++ app/(auth)/Signup.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SignupTab = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default SignupTab;
