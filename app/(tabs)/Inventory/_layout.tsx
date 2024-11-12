03c151fd5bfc016ff7cbc3c2376152db7adc7f38
Yeibin <yeibinkang@gmail.com>
2024-11-11 22:48:26 -0500

    added item list and detail pages. added functionality for creating, deleting, and updating for data

--- app/(tabs)/Inventory/_layout.tsx
+++ app/(tabs)/Inventory/_layout.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Stack } from 'expo-router';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ItemDetail from './ItemDetail';
+import AddItem from "./AddItem";
+import { NavigationContainer } from '@react-navigation/native';
+
+//const Stack = createNativeStackNavigator();
 
 const InventoryLayout = () => {
   return (
+    // <Stack>
+    //   <Stack.Screen name="index" options={{ headerShown: false }} />
+    //   <Stack.Screen name="ItemDetail" options={{ headerShown: false }} />
+    // </Stack>
+
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="ItemDetail" options={{ headerShown: false }} />
+      <Stack.Screen name="AddItem" options={{ headerShown: false }} />
     </Stack>
   );
 };
--- app/(tabs)/Inventory/index.tsx
+++ app/(tabs)/Inventory/index.tsx
@@ -1,82 +1,115 @@
-import React from 'react';
-import { Alert, Button, View } from 'react-native';
-import { router, useNavigation } from 'expo-router';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Alert, View } from 'react-native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import { supabase } from 'supabase';
-import { DataTable, IconButton } from 'react-native-paper';
-
+import { DataTable, IconButton, Button } from 'react-native-paper';
 
 const InventoryTab = () => {
-  // const navigation = useNavigation();
+  const [inventories, setInventories] = useState([]);
+  const navigation = useNavigation();
+
+  const [page, setPage] = useState(0);
+  const [numberOfItemsPerPageList] = useState([10]);
+  const [itemsPerPage, onItemsPerPageChange] = useState(numberOfItemsPerPageList[0]);
+  const from = page * itemsPerPage;
+  const to = Math.min((page + 1) * itemsPerPage, inventories.length);
+
+  const [sortAscending, setSortAscending] = useState<boolean>(true);
 
 
-const [inventories,setInventories] = useState<any | null>([]);
+  useEffect(() => {
+    setPage(0);
+  }, [itemsPerPage]);
 
-const navigation = useNavigation();
+  // Fetch data function
+  const fetchInventories = async () => {
+    try {
+      console.log('Fetching inventory data...');
+      const { data, error } = await supabase.from('Inventory').select('*');
+      if (error) throw error;
+      setInventories(data);
+    } catch (error) {
+      Alert.alert('Error fetching data', error.message);
+    }
+  };
 
-    const [page, setPage] = useState(0);
-    const [numberOfItemsPerPageList] = useState([10]);
-    const [itemsPerPage, onItemsPerPageChange] = useState(numberOfItemsPerPageList[0]);
-    const from = page * itemsPerPage;
-    const to = Math.min((page + 1) * itemsPerPage, inventories.length);
-
-    React.useEffect(() => {
-        setPage(0);
-    }, [itemsPerPage]);
-    
-useEffect(()=>{
-   const fetchInventories = async ()=>{
-     console.log('fetching...');
-     let { data, error } = await supabase.from('Inventory').select('*');
-     if(error){
-       Alert.alert('Error fetching data');
-     }
-     setInventories(data);
-   };
-  fetchInventories();
-  }, []);
+  // Use focus effect to refresh data when screen is focused
+  useFocusEffect(
+    React.useCallback(() => {
+      fetchInventories();
+    }, [])
+  );
 
 
+  async function pressAddItem() {
+    navigation.navigate('AddItem');
+  }
+
+  const sortedInventories = inventories
+    .slice()
+    .sort((inventory1, inventory2) =>
+      sortAscending ? inventory1.inventoryName.localeCompare(inventory2.inventoryName)
+        : inventory2.inventoryName.localeCompare(inventory1.inventoryName)
+    );
+
+
+  //todo: add Status sorting function
 
   return (
     <View>
-      <Button title="Go to Item Detail" onPress={() => router.push('/Inventory/ItemDetail')} />
       <DataTable>
-            <DataTable.Header>
-                <DataTable.Title style={{ flex: 3 }}>Item Name</DataTable.Title>
-                <DataTable.Title style={{ flex: 2 }}>Category</DataTable.Title>
-                <DataTable.Title style={{ flex: 1 }}>Qty</DataTable.Title>
-                <DataTable.Title style={{ flex: 1.5 }}>Unit</DataTable.Title>
-                <DataTable.Title style={{ flex: 0.5 }}></DataTable.Title>
-            </DataTable.Header>
+        <DataTable.Header>
+          <DataTable.Title sortDirection='descending' style={{ flex: 2, maxWidth: 50, justifyContent: 'center' }}>Status</DataTable.Title>
+          <DataTable.Title sortDirection={sortAscending ? 'ascending' : 'descending'}
+            onPress={() => setSortAscending(!sortAscending)} style={{ flex: 2, maxWidth: 120, justifyContent: 'center' }}>Item Name</DataTable.Title>
+          <DataTable.Title style={{ flex: 1, maxWidth: 30, justifyContent: 'center' }}>Qty</DataTable.Title>
+          <DataTable.Title style={{ flex: 2, maxWidth: 50, justifyContent: 'center' }}>Unit</DataTable.Title>
+          <DataTable.Title style={{ flex: 2, maxWidth: 80, justifyContent: 'center' }}>Stage</DataTable.Title>
+          <DataTable.Title style={{ flex: 1, maxWidth: 30, justifyContent: 'center' }}></DataTable.Title>
 
-            {inventories.slice(from, to).map((inventory: { id: React.Key | null | undefined; inventoryName: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; category: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; quantity: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; unit: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; }) => (
-                <DataTable.Row key={inventory.id} >
-                    <DataTable.Cell style={{ flex: 3 }}>{inventory.inventoryName}</DataTable.Cell>
-                    <DataTable.Cell style={{ flex: 2 }}>{inventory.category}</DataTable.Cell>
-                    <DataTable.Cell style={{ flex: 1 }}>{inventory.quantity}</DataTable.Cell>
-                    <DataTable.Cell style={{ flex: 1.5 }}>{inventory.unit}</DataTable.Cell>
-                    <IconButton
-                        icon="chevron-right-circle"
-                        size={20}
-                        // onPress={() => navigation.navigate('Inventory', { screen: 'InventoryDetailScreen', params: { inventory: inventories[(inventory.id) - 1] } })}
+        </DataTable.Header>
 
-                    />
-                </DataTable.Row>
-            ))}
+        {sortedInventories.slice(from, to).map((inventory) => (
+          <DataTable.Row key={inventory.id}>
+            <DataTable.Cell style={{ flex: 2, maxWidth: 50, justifyContent: 'center' }}>{inventory.status}</DataTable.Cell>
+            <DataTable.Cell style={{ flex: 2, maxWidth: 120, justifyContent: 'center' }}>{inventory.inventoryName}</DataTable.Cell>
+            <DataTable.Cell style={{ flex: 1, maxWidth: 30, justifyContent: 'center' }}>{inventory.quantity}</DataTable.Cell>
+            <DataTable.Cell style={{ flex: 2, maxWidth: 50, justifyContent: 'center' }}>{inventory.unit}</DataTable.Cell>
+            <DataTable.Cell style={{ flex: 2, maxWidth: 80, justifyContent: 'center' }}>{inventory.stage}</DataTable.Cell>
 
-            <DataTable.Pagination
-                page={page}
-                numberOfPages={Math.ceil(inventories.length / itemsPerPage)}
-                onPageChange={(page) => setPage(page)}
-                label={`${from + 1}-${to} of ${inventories.length}`}
-                numberOfItemsPerPageList={numberOfItemsPerPageList}
-                numberOfItemsPerPage={itemsPerPage}
-                onItemsPerPageChange={onItemsPerPageChange}
-                showFastPaginationControls
-                selectPageDropdownLabel={'Rows per page'}
-            />
-        </DataTable> 
+            <View style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              flex: 1,
+              maxWidth: 30
+            }}>
+              <IconButton
+                icon="chevron-right-circle"
+                size={20}
+                onPress={() => {
+                  navigation.navigate('ItemDetail', { screen: 'ItemDetail', inventory });
+                }}
+              />
+            </View>
+
+
+          </DataTable.Row>
+        ))}
+
+        <DataTable.Pagination
+          page={page}
+          numberOfPages={Math.ceil(inventories.length / itemsPerPage)}
+          onPageChange={(page) => setPage(page)}
+          label={`${from + 1}-${to} of ${inventories.length}`}
+          numberOfItemsPerPageList={numberOfItemsPerPageList}
+          numberOfItemsPerPage={itemsPerPage}
+          onItemsPerPageChange={onItemsPerPageChange}
+          showFastPaginationControls
+          selectPageDropdownLabel={'Rows per page'}
+        />
+      </DataTable>
+
+      <Button onPress={pressAddItem} mode='contained' buttonColor='#8ACB88' style={{}}>Add item</Button>
     </View>
   );
 };
