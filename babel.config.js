7918f1f7f11b0a3145a01262a2a5b0b3e7eeae13
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2024-11-14 16:26:04 -0500

    attempt to repair dependency issues

--- babel.config.js
+++ babel.config.js
@@ -4,10 +4,12 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
     plugins: [
       [
+        'module:react-native-dotenv',
         'module-resolver',
         {
           alias: {
             '@assets': './assets',
+            '@context':'./app/ctx',
             '@components': './app/components',
             '@screens': './app/screens',
             '@stores': './app/stores',
